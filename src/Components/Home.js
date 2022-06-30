72d6187fdd5636a08bc7ab49ed6d7f45d88bf347
ador <rashedulhaque111@gmail.com>
2022-06-30 18:22:06 +0600

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-blue-700 text-3xl'>Welcome to Daily Task Manager</h1>
+
+        </div>
+    );
+};
+
+export default Home;
