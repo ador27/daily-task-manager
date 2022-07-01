3845fb82d39fa55483a1d8f217796b937f5dd026
ador <rashedulhaque111@gmail.com>
2022-07-01 08:32:34 +0600

    firebase hosting complete

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD2dAmYiA31dVCfMl6pbNeMFMzzEPwZZDU",
+    authDomain: "task-manager-d1350.firebaseapp.com",
+    projectId: "task-manager-d1350",
+    storageBucket: "task-manager-d1350.appspot.com",
+    messagingSenderId: "168427224849",
+    appId: "1:168427224849:web:2c23ad46eaaf49bbd9d6b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
